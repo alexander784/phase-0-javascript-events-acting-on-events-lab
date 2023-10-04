a967475156a2fc07f558baa168b1218a5918bb29
alexander <alexandernyaga1999@gmail.com>
2023-10-04 11:43:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Your code here
 const dodger = document.getElementById('dodger');
-function moveDodgerLeft() {
-  const leftNumbers = dodger.style.left.replace('px', '');
-  const left = parseInt(leftNumbers, 10);}
-
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;}
+function moveDodgerLeft(){
+  const  leftNumbers = dodger.style.left.replace("px","");
+  const left = parseInt(leftNumbers,10);
+ if (left > 0){ 
+  dodger.style.left = `${left - 1}px`;
+ }
+}
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
